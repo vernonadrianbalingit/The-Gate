fdd9f67907d3b365be388726e34a3ffc741f8bdc
itsMtal <145627808+RomelAleman@users.noreply.github.com>
2025-12-17 12:07:36 -0800

    Add background and audio

---
+++ Assets/romel/Scripts/OnCityEnter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCityEnter : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            ScrCityHealth cityHealth = FindObjectOfType<ScrCityHealth>();
+            if (cityHealth != null)
+            {
+                cityHealth.cityHealth -= 100; // Decrease city health by 10 on collision
+                Debug.Log("City Health: " + cityHealth.cityHealth);
+            }
+            Destroy(collision.gameObject); // Destroy the enemy after it hits the city
+        }
+    }
+}
